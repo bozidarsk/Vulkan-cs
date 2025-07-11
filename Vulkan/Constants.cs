d95ba26cd4ee98916f9122cf0e32edc3d43d9a42
bozidarsk <bozidarsk@proton.me>
2025-07-11 22:03:49 +0300

    Unused constants.

--- Vulkan/Constants.cs
+++ Vulkan/Constants.cs
@@ -38,14 +38,6 @@ internal static class Constants
 	#endif
 	;
 
-	public const int shaderc_vertex_shader = 0;
-	public const int shaderc_fragment_shader = 1;
-	public const int shaderc_compute_shader = 2;
-	public const int shaderc_geometry_shader = 3;
-	public const int shaderc_tess_control_shader = 4;
-	public const int shaderc_tess_evaluation_shader = 5;
-	public const int shaderc_compilation_status_success = 0;
-
 	public const int VK_MAX_EXTENSION_NAME_SIZE = 256;
 	public const int VK_MAX_DESCRIPTION_SIZE = 256;
 	public const int VK_MAX_PHYSICAL_DEVICE_NAME_SIZE = 256;
