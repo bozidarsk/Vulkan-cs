f039014659bff102e204fa22852fe2d0cbe83583
bozidarsk <bozidarsk@proton.me>
2026-06-30 13:31:34 +0300

    Compute the correct depth value in fragment shader.

--- Shaders/default.frag.hlsl
+++ Shaders/default.frag.hlsl
@@ -5,14 +5,5 @@
 
 float4 main(Fragment input)
 {
-	float depth = input.position.z / input.position.w;
-
-	if (depth < 0)
-		return float4(1, 0, 0, 1);
-	else if (depth > 1.1)
-		return float4(0, 1, 0, 1);
-	else if (depth > 1)
-		return float4(0, 0, 1, 1);
-	else
-		return float4(depth.xxx, 1);
+	return float4(input.position.zzz, 1);
 }
